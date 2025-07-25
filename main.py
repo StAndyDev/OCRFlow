bd2b3ec98033a69ee4cca50c68da6eb08aba7858
sitraka andy <sitrakaandrianirina777@gmail.com>
2025-07-26 00:02:24 +0200

    initial project setup

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    image_path = "data/raw/exemple.jpg"
+
+    # Étape 1 : prétraitement
+    # Étape 2 : détection des zones
+    # Étape 3 : OCR sur chaque zone
+    # Étape 4 : Structuration
+    # Export ou affichage
+
+if __name__ == "__main__":
+    main()
